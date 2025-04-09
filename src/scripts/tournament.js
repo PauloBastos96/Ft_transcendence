3dbe657d2c530f30d8fd1e150c3315e8d4b16c1e
mortinso <89762267+mortinso@users.noreply.github.com>
2025-04-09 18:38:40 +0100

    Now accepting usernames and blocking repeated usernames. Other improvements: 	- Hides the 'option-section' div when the tournament size has been selected 	- Go back button working
    ToDo:
    	- Force player1 to have the users username
    	- Actually create a tournament bracket and handle matchmaking lol

--- src/scripts/tournament.js
+++ src/scripts/tournament.js
@@ -1,4 +1,5 @@
 // Player bubble colors
+// TODO: change colors
 const BUBBLE_COLORS = [
 	'#c70b0b',	// Red
 	'#ee940c',	// Orange
@@ -10,21 +11,52 @@ const BUBBLE_COLORS = [
 	'#e23ac6',	// Pink
 ];
 
-let playerList;
+let players = new Set();
+let playerList, optionSection, playerSection, createButton;
 
-function initTournament(players) {
+function initTournament(playerNum) {
+	// TODO: force Player1 to have the users username. It shouldn't be an <input>
 	playerList = document.getElementById('playerList');
+	optionSection = document.getElementById('option-section');
+	playerSection = document.getElementById('player-section');
+	createButton = document.getElementById('createButton');
 
 	playerList.textContent = '';
-	for (let i = 1; i <= players; i++) {
+	for (let i = 1; i <= playerNum; i++) {
 		const player = document.createElement('input');
 		player.id = `player${i}`;
 		player.className = 'player-name';
 		player.placeholder = `Player ${i}`;
-		player.style.placeholder.color
-		player.style.backgroundColor = BUBBLE_COLORS[(i - 1) % BUBBLE_COLORS.length] 
+		player.style.backgroundColor = BUBBLE_COLORS[(i - 1) % BUBBLE_COLORS.length];
 		playerList.appendChild(player);
 	}
-	// Create set of players
-	// Add a div for each player 
+	// Prevent automatic activation
+	if (playerNum) {
+		optionSection.style.display = 'none';
+		playerSection.style.display = 'block';
+		createButton.onclick = function() { createTournament(`${playerNum}`); };
+	}
+}
+
+function goBack() {
+	optionSection.style.display = 'block';
+	playerSection.style.display = 'none';
+}
+
+function createTournament(playerNum) {
+	players.clear();
+	// This loop gets the player names, and forbids repeated names. It also uses the placeholder if no name has been set
+	for (let i = 1; i <= playerNum; i++) {
+		let username = document.getElementById(`player${i}`).value;
+		if (!username)
+			username = `Player ${i}`;
+		if (players.has(username)) {
+			alert(`No repeated names allowed: ${username}`);
+			players.clear();
+			return;
+		}
+		players.add(username);
+	}
+	alert("W");
+	console.log("players: ", players);
 }
